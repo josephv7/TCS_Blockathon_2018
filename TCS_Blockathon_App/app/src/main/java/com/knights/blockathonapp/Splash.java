b749801b1b5406aa5aedfebdc9e025c6adbc20a3
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-10-03 21:30:59 +0530

    added shared preference checking in splash and corresponding intent, new activities

--- TCS_Blockathon_App/app/src/main/java/com/knights/blockathonapp/Splash.java
+++ TCS_Blockathon_App/app/src/main/java/com/knights/blockathonapp/Splash.java
@@ -1,23 +1,37 @@
 package com.knights.blockathonapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sharedPreferences = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent homeIntent = new Intent(Splash.this,Home.class);
-                startActivity(homeIntent);
+                if(sharedPreferences.getBoolean("loggedin",false)){
+                    Intent homeIntent = new Intent(Splash.this,Home.class);
+                    startActivity(homeIntent);
+                    finish();
+                }else{
+                    Intent loginIntent = new Intent(Splash.this,Login.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+
+
 
             }
         },3000);
