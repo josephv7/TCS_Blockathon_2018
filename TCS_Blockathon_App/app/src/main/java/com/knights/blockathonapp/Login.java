b44e53a07daf587b35b86e6618ace1fa15b8af9a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-10-03 23:29:06 +0530

    code for login added

--- TCS_Blockathon_App/app/src/main/java/com/knights/blockathonapp/Login.java
+++ TCS_Blockathon_App/app/src/main/java/com/knights/blockathonapp/Login.java
@@ -1,13 +1,65 @@
 package com.knights.blockathonapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    EditText username,password;
+    Button login;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myRef.child("patients").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                       if( dataSnapshot.child(username.getText().toString()).exists()){
+                           if(dataSnapshot.child(username.getText().toString()).getValue(String.class).equals(password.getText().toString())){
+                               Intent homeIntent = new Intent(Login.this, Home.class);
+                               startActivity(homeIntent);
+                               finish();
+
+//                               TODO add other false condition checkings and proper messages
+                           }
+                       }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+
     }
 }
